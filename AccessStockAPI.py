dab24ebc8ed3d2dc4d40b69cbd103f6de04d9dae
Kim J Kubik <kkubik7@hotmail.com>
2021-04-22 19:41:37 -0500

    Create AccessStockAPI.py
    Accessing Stock API at polygon.com

---
+++ AccessStockAPI.py
@@ -0,0 +1,25 @@
+import datetime
+
+from polygon import RESTClient
+
+def ts_to_datetime(ts) -> str:
+    return datetime.datetime.fromtimestamp(ts / 1000.0).strftime('%Y-%m-%d %H:%M')
+
+def main():
+    key = "ooA1BDEnla5owTATGDfKAuIi6puKOitC"
+
+    # RESTClient can be used as a context manager to facilitate closing the underlying http session
+    # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
+    with RESTClient(key) as client:
+        from_ = "2020-01-11"
+        to = "2021-01-15"
+        resp = client.stocks_equities_aggregates("AMD", 1440, "minute", from_, to, unadjusted=False)
+
+        print(f"Minute aggregates for {resp.ticker} between {from_} and {to}.")
+
+        for result in resp.results:
+            dt = ts_to_datetime(result["t"])
+            print(f"{dt}\n\tO: {result['o']}\n\tH: {result['h']}\n\tL: {result['l']}\n\tC: {result['c']} ")
+
+if __name__ == '__main__':
+    main()
